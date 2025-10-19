3f2d8e23ab6b3d953dcff077f9d69aa46453be99
muhammedaliugur <ugurmuhammedali58@gmail.com>
2025-10-19 22:42:08 +0300

    ALLOWED_HOSTS hatası düzeltildi

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
