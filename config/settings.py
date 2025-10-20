0353f111d29ec2d3f49da88bcc672c96bd28a259
muhammedaliugur <ugurmuhammedali58@gmail.com>
2025-10-20 14:13:48 +0300

    düzeltme

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,18 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")  # "*" veya "agd
 
 # Application definition
 
+import os
+
+if os.environ.get('RENDER'):
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = 'agdhaber34@gmail.com'
+    EMAIL_HOST_PASSWORD = 'altt avlu unas bvat'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
